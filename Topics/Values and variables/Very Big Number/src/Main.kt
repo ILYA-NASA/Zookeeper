e45aa1bc1b903a9ed3578defae89c47a85973084
ILYA.NASA <il.nasa@outlook.com>
2022-12-15 09:40:52 +0300

    my solution daily task

---
+++ Topics/Values and variables/Very Big Number/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // do not touch the lines above
+    // declare and initialize a variable here
+
+    // val bigNumber = 100_000_000 // Magic number
+    val bigNumber = "100_000_000"
+
+    // do not touch the lines below
+    println(bigNumber)
+}
