764071bfde77ded9fb42c54162c35231701a049f
ILYA.NASA <il.nasa@outlook.com>
2022-12-20 18:02:01 +0300

    study progress import

---
+++ Topics/The classification of basic types/Max values of integer types/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println(Byte.MAX_VALUE) //  127
+    println(Short.MAX_VALUE) // 32767
+    println(Int.MAX_VALUE) //   2147483647
+    println(Long.MAX_VALUE) //  9223372036854775807
+}
